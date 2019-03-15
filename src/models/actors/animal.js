d0128ed1e998e4c4bd550d90c50df4900eceebcd
carles <carles.sanz.contractor@dev.bbva.com>
2019-03-15 14:44:56 +0100

    feature: create animal class as any animal interface

--- src/models/actors/animal.js
+++ src/models/actors/animal.js
@@ -1,7 +1,40 @@
 class Animal {
   constructor() {
     this._timeToDeath = undefined
+    this._gender = undefined
+    this._isFertile = undefined
+    this._energyIndex = undefined
+    this._hungryIndex = undefined
+    this._diseases = []
+    this._wounds = []
+    this._isPregnant = undefined
+    this._breedings = []
   }
+
+  getTimeToDeath() {}
+  setTimeToDeath() {}
+  getGender() {}
+  setGender() {}
+  getIsFertile() {}
+  setIsFertile() {}
+  getEnergyIndex() {}
+  setEnergyIndex() {}
+  getHungryIndex() {}
+  setHungryIndex() {}
+  getDiseases() {}
+  setDiseases() {}
+  getWounds() {}
+  setWounds() {}
+  getIsPregnant() {}
+  setIsPregnant() {}
+  getBreedings() {}
+  setBreedings() {}
+  eat() {}
+  sleep() {}
+  rest() {}
+  run() {}
+  walk() {}
+  reproduce() {}
 }
 
 module.exports = Animal
